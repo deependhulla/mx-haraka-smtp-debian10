ff80c84753385a83dcec9bfe7367610bc468eaaf
Deepen Dhulla <deepen@deependhulla.com>
2020-03-26 22:54:59 +0530

    update 2020-03-26

--- rootdir/opt/haraka-inbound/stop-inbound.sh
+++ rootdir/opt/haraka-inbound/stop-inbound.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-kill -9 `cat /var/run/haraka-inbound.pid` 
-/bin/rm /var/run/haraka-inbound.pid 
+kill -9 `cat /var/run/haraka-inbound.pid 2>/dev/null` 2>/dev/null 1>/dev/null 
+/bin/rm /var/run/haraka-inbound.pid 2>/dev/null 
+
+echo " Haraka Stopped  "
 
