f39d326107fe4178ab7d6a0f3e9e838152354971
Deepen Dhulla <deepen@deependhulla.com>
2020-04-20 18:44:45 +0530

    updated sed for hostname
    updated sed lines for hostname replace.

--- 03-letencrypt-ssl-setup.sh
+++ 03-letencrypt-ssl-setup.sh
@@ -2,11 +2,16 @@
 
 
 ##certbot certonly -d `hostname` --standalone --agree-tos --email yourmail@example.com
-#certbot certonly -d `hostname` --standalone --agree-tos --email postmaster@`hostname`
+certbot certonly -d `hostname` --standalone --agree-tos --email postmaster@`hostname`
 
  systemctl stop cron
 
 echo "MAILTO=\"\"" >> /var/spool/cron/crontabs/root 
 echo "30 2 * * 1 /usr/local/src/cert-renew-and-restart.sh" >> /var/spool/cron/crontabs/root  
 systemctl start cron
+
+sed -i "s/powermail\.mydomainname\.com/`hostname`/" /etc/postfix/main.cf
+sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/tls.ini
+sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/host_list
+sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/me
 
