93af6da2b33f7d5c9937c5eaf2cfb4e5ad4964f5
Deepen Dhulla <deepen@deependhulla.com>
2020-06-23 15:50:52 +0530

    updated hostname -f
    updated hostname -f

--- 03-letencrypt-ssl-setup.sh
+++ 03-letencrypt-ssl-setup.sh
@@ -2,7 +2,7 @@
 
 
 ##certbot certonly -d `hostname` --standalone --agree-tos --email yourmail@example.com
-certbot certonly -d `hostname` --standalone --agree-tos --email postmaster@`hostname`
+certbot certonly -d `hostname -f` --standalone --agree-tos --email postmaster@`hostname -f`
 
  systemctl stop cron
 
@@ -10,8 +10,8 @@ echo "MAILTO=\"\"" >> /var/spool/cron/crontabs/root
 echo "30 2 * * 1 /usr/local/src/cert-renew-and-restart.sh" >> /var/spool/cron/crontabs/root  
 systemctl start cron
 
-sed -i "s/powermail\.mydomainname\.com/`hostname`/" /etc/postfix/main.cf
-sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/tls.ini
-sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/host_list
-sed -i "s/powermail\.mydomainname\.com/`hostname`/" /opt/haraka-inbound/config/me
+sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /etc/postfix/main.cf
+sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /opt/haraka-inbound/config/tls.ini
+sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /opt/haraka-inbound/config/host_list
+sed -i "s/powermail\.mydomainname\.com/`hostname -f`/" /opt/haraka-inbound/config/me
 
